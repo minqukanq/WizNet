c630e48ef1a447866f86b1976a1252277abf5f7c
callor <callor@callor.com>
2017-06-26 19:45:44 +0900

    2017-06-26 dashboard

--- routes/mainController.js
+++ routes/mainController.js
@@ -93,20 +93,21 @@ module.exports =(app,pm25VO)=>{
 	
 	app.post("/pm25/getlast",(req,res)=>{
 
-		Date.prototype.yyyymmdd = function(){
-		    var yyyy = this.getFullYear().toString();
-		    var mm = (this.getMonth() + 1).toString();
-		    var dd = this.getDate().toString();
-		    return yyyy +"-"+(mm[1] ? mm : '0'+mm[0])+"-"+(dd[1] ? dd : '0'+dd[0]);
-		};
-		 
-		var nowDate = (new Date).yyyymmdd();
-		console.log(nowDate);
+//		Date.prototype.yyyymmdd = function(){
+//		    var yyyy = this.getFullYear().toString();
+//		    var mm = (this.getMonth() + 1).toString();
+//		    var dd = this.getDate().toString();
+//		    return yyyy +"-"+(mm[1] ? mm : '0'+mm[0])+"-"+(dd[1] ? dd : '0'+dd[0]);
+//		};
+//		 
+//		var nowDate = (new Date).yyyymmdd();
+//		console.log(nowDate);
 
 		pm25VO.findOne()
 			.sort({PM25_DATE:-1})
 			.sort({PM25_TIME: -1})
 			.exec((err,data)=>{
+				res.status(200);
 				res.json(data);
 		});
 	})
